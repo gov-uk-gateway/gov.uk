1d2a67dec70c601c426daf064d3d92235ec6f747
Valentyn Bobyr <valko_bob@ukr.net>
2024-12-11 16:57:43 +0200

    fix #17

---
+++ vat-through-software/script.js
@@ -0,0 +1,28 @@
+const returnTabHeader2024 = document.getElementById('tab_past-payments-2024');
+const returnTabHeader2023 = document.getElementById('tab_past-payments-2023');
+const returnTabHeader2022 = document.getElementById('tab_past-payments-2022');
+
+const selectTabSelector = 'govuk-tabs__list-item--selected';
+const hiddenTabSelector = 'govuk-tabs__panel--hidden';
+
+function handleTab(visibleYear, unvisibleYears) {
+  document.getElementById(`tab_past-payments-${visibleYear}`).parentElement.classList.add(selectTabSelector);
+  document.getElementById(`past-payments-${visibleYear}`).classList.remove(hiddenTabSelector);
+
+  unvisibleYears.forEach(unvisibleYear => {
+    document.getElementById(`tab_past-payments-${unvisibleYear}`).parentElement.classList.remove(selectTabSelector);
+    document.getElementById(`past-payments-${unvisibleYear}`).classList.add(hiddenTabSelector);
+  })
+}
+
+returnTabHeader2024.addEventListener('click', (e) => {
+  handleTab('2024', ['2023', '2022'])
+})
+
+returnTabHeader2023.addEventListener('click', (e) => {
+  handleTab('2023', ['2024', '2022'])
+})
+
+returnTabHeader2022.addEventListener('click', (e) => {
+  handleTab('2022', ['2023', '2024'])
+})
